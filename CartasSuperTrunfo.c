cb0f4e79f0d8c9e78918a2997628867708990c93
herclys <herclystorres64@gmail.com>
2025-02-12 18:58:01 +0000

    versao quase final

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -30,12 +30,12 @@ int main() {
     scanf("%d", &pontos_turisticos); //Lê o número de pontos turísticos
 
     //exibição dos dados das cartas
-    printf()
-    printf()
-    printf()
-    printf()
-    printf()
-    printf()
-    printf()
+    printf("\ndados da cidade cadastrada: \n");
+    printf("codigo:%d\n", codigo);
+    printf("nome: %s\n",nome);
+    printf("populacao:%d\n",populacao);
+    printf("area:%.2f km²\n", area);
+    printf("pib: %.2f bilhoes \n", pib);
+    printf("pontos turisticos:%.2f\n", pontos_turisticos);
 return 0;
 }
